8cb703dcf2d6ea7e34a2b2afa4745a57ef098c70
christafayereyes <christafayereyes@gmail.com>
2021-12-08 10:07:50 -0600

    Added test cases for listing and getting recipe. Tests passed.

--- test/cookbook.test.js
+++ test/cookbook.test.js
@@ -14,13 +14,23 @@ describe('Cookbook', () => {
 
   describe('Listing recipes', () => {
     test('should allow the recipes to be listed', () => {
+      const spagRecipe = new Cookbook();
+            
+      spagRecipe.addRecipe('spaghetti', ['beef', 'pasta noodles', 'mushroom', 'tomato sauce', 'onion', 'cheese']);
 
+      const mySpagRecipe = spagRecipe.listRecipes();
+      expect(mySpagRecipe).toEqual(['spaghetti']);
     });
   });
 
   describe('Retrieving a recipe', () => {
     test('should allow the ingredients for a recipe to be retrieved', () => {
-
+      const spagRecipe = new Cookbook();
+            
+      spagRecipe.addRecipe('spaghetti', ['beef', 'pasta noodles', 'mushroom', 'tomato sauce', 'onion', 'cheese']);
+      const mySpagRecipe = spagRecipe.getRecipe(['spaghetti']);
+      expect(mySpagRecipe).toEqual(['beef', 'pasta noodles', 'mushroom', 'tomato sauce', 'onion', 'cheese']);
+      
     });
   });
 
