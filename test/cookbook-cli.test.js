c55dd719029d4099b8826f65a9bdcab3f276aae1
christafayereyes <christafayereyes@gmail.com>
2021-12-08 16:04:18 -0600

    Added test cases for add, list, get and remove for the  conditions displaying warning message if recipe does not exists

--- test/cookbook-cli.test.js
+++ test/cookbook-cli.test.js
@@ -3,17 +3,28 @@ const { CookbookCli } = require('../src/cookbook-cli');
 
 describe('CookbookCli', () => {
   describe('Adding recipes', () => {
-    test('should accept the recipe information and display the correct message', () => {
+    test('should accept the recipe information if recipe does not exist and display the correct message', () => {
       const myrecipeCookbook = new Cookbook();
       const myCookbookCli = new CookbookCli(myrecipeCookbook);
 
       const message = myCookbookCli.add('spaghetti', ['beef', 'pasta noodles', 'mushroom', 'tomato sauce', 'onion', 'cheese']);
-
-      expect(message).toEqual(`Successfully added the following recipe: spaghetti`);
+      expect(message).toBe(false);
+      
     });
-  });
 
-  describe('Listing recipes', () => {
+    test('should not accept the recipe information if recipe exists and display the correct message', () => {
+      const myrecipeCookbook = new Cookbook();
+      const myCookbookCli = new CookbookCli(myrecipeCookbook);
+  
+      myCookbookCli.add('spaghetti', ['beef', 'pasta noodles', 'mushroom', 'tomato sauce', 'onion', 'cheese']);
+      const message = myCookbookCli.add('spaghetti', ['beef', 'pasta noodles', 'mushroom', 'tomato sauce', 'onion', 'cheese']);
+      expect(message).toBe(true);
+
+    });
+  })
+  
+
+  describe('Listing recipes if there are any existing recipes, otherwise, display warning message', () => {
     test('should display the correct message listing all of the recipe names', () => {
       const myrecipeCookbook = new Cookbook();
       const myCookbookCli = new CookbookCli(myrecipeCookbook);
@@ -23,9 +34,18 @@ describe('CookbookCli', () => {
 
       expect(message).toEqual(`You have the following recipes: spaghetti`);
     });
+
+    test('should display warning message if no recipe exists', () => {
+      const myrecipeCookbook = new Cookbook();
+      const myCookbookCli = new CookbookCli(myrecipeCookbook);
+
+      const message = myCookbookCli.list();
+
+      expect(message).toEqual(`Oops! No recipe is existing on this cookbook.`);
+    });
   });
 
-  describe('Retrieving a recipe', () => {
+  describe('Retrieving a recipe if recipe exists, otherwise, display warning message', () => {
     test('should display the ingredients required to make the specified recipe', () => {
       const myrecipeCookbook = new Cookbook();
       const myCookbookCli = new CookbookCli(myrecipeCookbook);
@@ -35,17 +55,37 @@ describe('CookbookCli', () => {
 
       expect(message).toEqual(`The ingredients for spaghetti are: beef,pasta noodles,mushroom,tomato sauce,onion,cheese`);
     });
+
+    test('should display warning message if recipe does not exist', () => {
+      const myrecipeCookbook = new Cookbook();
+      const myCookbookCli = new CookbookCli(myrecipeCookbook);
+  
+      myCookbookCli.add('spaghetti', ['beef', 'pasta noodles', 'mushroom', 'tomato sauce', 'onion', 'cheese']);
+      const message = myCookbookCli.get('pizza');
+  
+      expect(message).toEqual(`Oops! pizza's recipe does not exist on this cookbook.`);
+      
+    });  
   });
 
-  describe('Deleting a recipe', () => {
-    test('should accept the recipe name and display the correct message', () => {
+  describe('Deleting a recipe if recipe exists, otherwise, display warning message', () => {
+    test('should delete the recipe name and display the correct message', () => {
       const myrecipeCookbook = new Cookbook();
       const myCookbookCli = new CookbookCli(myrecipeCookbook);
 
       myCookbookCli.add('spaghetti', ['beef', 'pasta noodles', 'mushroom', 'tomato sauce', 'onion', 'cheese']);
       const message = myCookbookCli.remove('spaghetti');
 
-      expect(message).toEqual(`Successfully removed the following recipe: spaghetti`);
+      expect(message).toBe(true);
     });
+
+    test('should display warning message if recipe does not exist', () => {
+      const myrecipeCookbook = new Cookbook();
+      const myCookbookCli = new CookbookCli(myrecipeCookbook);
+
+      const message = myCookbookCli.remove('spaghetti');
+      expect(message).toBe(false);
+    });
+
   });
 });
